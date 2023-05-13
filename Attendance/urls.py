0835109d7a4fb322f7f3af46d130d1b7cb97e3ae
M Taha Makhdoom <taha.makhdoom10@gmail.com>
2023-05-14 00:14:00 +0500

    Revert "can now add and delete attendance"
    This reverts commit 4e6418f375db291bc45bc1d7b3b5ef02a813d9c5.

--- Attendance/urls.py
+++ Attendance/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
     path('', views.main , name='home'),
     path('another/', views.another , name='another'),
     path('save_attendance/', views.save_attendance, name='save_attendance'),
-    path('add_New_Date/', views.add_New_Date, name='add_New_Date'),
-    path('add_New_Attendance/', views.add_New_Attendance, name='add_New_Attendance'),
-    path('delete_attendance_of_one_date/', views.delete_attendance_of_one_date, name='delete_attendance_of_one_date'),
+    path('add_New_Date/', views.save_attendance, name='add_New_Date'),
 ]
